a74e28cfa6562a22fa1e95d174a110c8d8949926
Devon1129 <koistu0501@gmail.com>
2015-05-20 23:37:10 +0800

    SQLiteAndMeun

---
+++ SQLiteAndMENU/src/com/example/sqlitetest3/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.sqlitetest3;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends Activity {
+	private ListView myListView;
+	private EditText myEditText;
+	MySQLiteOpenHelper myHelper;
+	private Cursor myCursor;
+	private int _ID;
+	protected static final int MENU_ADD = Menu.FIRST;
+	protected static final int MENU_EDIT = Menu.FIRST + 1;
+	protected static final int MENU_DELETE = Menu.FIRST + 2;
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		//(群組，識別碼，按鈕出現的順序，顯示的文字)
+		menu.add(0, MENU_ADD, 0, R.string.AddButton);
+		menu.add(0, MENU_EDIT, 1, R.string.EditButton);
+		menu.add(0, MENU_DELETE, 2, R.string.DeleteButton);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+		switch(item.getItemId()){
+		case MENU_ADD :
+			this.addTodo();
+			break;
+		case MENU_EDIT:
+			this.editTodo();
+			break;
+		case MENU_DELETE:
+			this.deleteTodo();
+			break;
+		}
+		return true;
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//建立連結
+		myListView = (ListView)findViewById(R.id.listView1);
+		myEditText = (EditText)findViewById(R.id.editText1);
+		
+		myHelper = new MySQLiteOpenHelper(this);
+		
+		myCursor = myHelper.select();
+		
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+				this, 
+				R.layout.list, 
+				myCursor, 
+				new String[] {myHelper.Field_Text}, 
+				new int[] {R.id.listTextView1});
+		
+		myListView.setAdapter(adapter);
+
+		//監聽ListView是否有被點擊
+		myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				//cursor移動到被點擊的位置上
+				myCursor.moveToPosition(arg2);
+				//cursor抓取第0個欄位給 _ID.
+				_ID = myCursor.getInt(0);
+				//cursor抓取第1個欄位給EditText.
+				myEditText.setText(myCursor.getString(1));				
+			}
+		});
+			
+		//監聽 ListView是否有被選取.
+		myListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				//獲得一個 SQLiteCursor類別的物件，指向 SQLite資料庫查詢的結果.
+				SQLiteCursor sc = (SQLiteCursor) arg0.getSelectedItem();
+				//取得該筆資料的第0個欄位內容，資料型態為整數.
+				_ID = sc.getInt(0);
+				//取得該筆資料的第1個欄位內容，資料型態為字串，將資料填入 myEditText中.
+				myEditText.setText(sc.getString(1));
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
+	private void addTodo() {
+		if(myEditText.getText().toString().equals("")){
+			return;
+		}else{
+			//呼叫 SQLite的 insert()方法，將輸入欄位內的資料插入資料庫中.
+			myHelper.insert(myEditText.getText().toString());
+			
+			//重新查詢資料庫的內容.
+			myCursor.requery();
+			//重新顯示 ListView的內容 +清除輸入欄位的資料+ _ID歸0.
+			//invalidateViews:讓View失效。
+			//會導致android重畫畫面(重新顯示 ListView的內容)。
+			myListView.invalidateViews();
+			myEditText.setText("");
+			_ID = 0;
+		}
+	}
+	
+
+	private void editTodo() {
+		if(myEditText.getText().toString().equals("")){
+			return;
+		}else{
+			myHelper.update(_ID, myEditText.getText().toString());
+			
+			//重新查詢資料庫的內容
+			myCursor.requery();
+			//重新顯示 ListView的內容 + 清除輸入欄位的資料+ _ID歸0.
+			myListView.invalidateViews();
+			myEditText.setText("");
+			_ID = 0;
+		}
+	}
+
+	private void deleteTodo() {
+		if(_ID == 0){
+			return;
+		}else{
+			//呼叫 SQLite的delete()方法.
+			myHelper.delete(_ID);
+			
+			//重新查詢資料庫的內容.
+			myCursor.requery();
+			//重新顯示 ListView的內容+ 清除輸入欄位的資料+ _ID歸0.
+			myListView.invalidateViews();
+			myEditText.setText("");
+			_ID = 0;
+		}
+	}
+}//end class MainActivity.
---
+++ SQLiteAndMENU/src/com/example/sqlitetest3/MySQLiteOpenHelper.java
@@ -0,0 +1,74 @@
+package com.example.sqlitetest3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+	private static final String DB_Name = "todo.db";
+	private static final String Table_Name = "todo_Table";
+	protected static final String Field_Id = "_id";
+	protected static final String Field_Text = "todo_text";
+	
+	public MySQLiteOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MySQLiteOpenHelper(Context context){
+		super(context, DB_Name, null, 1);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String sql = "CREATE TABLE " + Table_Name + 
+				" ( " + 
+				Field_Id + " INTEGER primary key autoincrement, " + 
+				Field_Text + " text);";
+		db.execSQL(sql);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		//[fix] "EXISTS" 後面沒有接"空格"以用來與table name分隔.
+		String sql = "DROP TABLE IF EXISTS " + Table_Name;
+		db.execSQL(sql);
+		onCreate(db);
+	}
+	
+	public Cursor select(){
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.query(Table_Name, null, null, null, null, null, null);
+		return cursor;
+	}
+	
+	public long insert(String text){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		//要放在資料庫的哪一個欄位，內容.
+		cv.put(Field_Text, text);
+		long row = db.insert(Table_Name, null, cv); 
+		return row;
+	}
+	
+	public void delete(int id){
+		SQLiteDatabase db = this.getWritableDatabase();
+		String where = Field_Id + "=?";
+		String[] whereValue = {Integer.toString(id)};
+		db.delete(Table_Name, where, whereValue);
+	}
+	
+	public void update(int id, String text){
+		SQLiteDatabase db = this.getWritableDatabase();
+		String where = Field_Id + "=?";
+		String[] whereVale = {Integer.toString(id)};
+		
+		ContentValues cv = new ContentValues();
+		cv.put(Field_Text, text);
+		db.update(Table_Name, cv, where, whereVale);
+	}
+}//end class MySQLitenOpenHelper. 
