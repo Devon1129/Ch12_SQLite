49d14723c61f7de26eaa07a5b626301c25b2ec5f
Devon1129 <koistu0501@gmail.com>
2015-05-26 17:23:56 +0800

    SimpleCursorAdapter -在 AndroidMainfest加入權限. -多加一筆資料測試顯示的情形

--- Ex07_SimpleCursorAdapter/src/com/example/ex07_simplecursoradapter/MainActivity.java
+++ Ex07_SimpleCursorAdapter/src/com/example/ex07_simplecursoradapter/MainActivity.java
@@ -13,14 +13,18 @@ public class MainActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		//指向通訊錄數據庫的 cursor，來獲得資料來源。
 		Cursor cur = getContentResolver().query(People.CONTENT_URI, null, null, null, null);
         startManagingCursor(cur);
         
-        
+        //連接 Devices設備(手機)裡的資訊。
         ListAdapter adapter = 
     		new SimpleCursorAdapter(
-    				this,android.R.layout.simple_list_item_1,cur,
-    				new String[] {People.NAME}, new int[] {android.R.id.text1});
+    				this,android.R.layout.simple_list_item_2,cur,
+    				new String[] {People.NAME, People.NOTES}, 
+    				new int[] {android.R.id.text1, android.R.id.text2});
+
         setListAdapter(adapter);
 	}
 }
+//http://www.cnblogs.com/devinzhang/archive/2012/01/20/2328334.html
