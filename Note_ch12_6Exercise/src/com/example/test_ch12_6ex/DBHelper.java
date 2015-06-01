6e3aee2aba2eaf901e954c0556d7395a75df6466
Devon1129 <koistu0501@gmail.com>
2015-06-01 13:31:54 +0800

    Note_ch12_6QueryRec -FinRec() -onResume() & onPanse()

---
+++ Note_ch12_6Exercise/src/com/example/test_ch12_6ex/DBHelper.java
@@ -0,0 +1,98 @@
+package com.example.test_ch12_6ex;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DBHelper extends SQLiteOpenHelper {
+
+	private static final String Table_Name = "Cus";
+
+	public DBHelper(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DBHelper(Context context){
+		super(context, "Campany.db", null, 1);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String create_Table = "create table " + Table_Name +
+				"( " + 
+				" cusNo VARCHAR(10) NOT NULL, " +
+				" cusNa VARCHAR(20) NOT NULL, " + 
+				" cusPho VARCHAR(30), " + 
+				" cusAdd VARCHAR(50), primary key(cusNo));";
+		db.execSQL(create_Table);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		String sql = (" DROP TABLE IF EXISTS " + Table_Name); 
+		db.execSQL(sql);
+		onCreate(db);
+		
+	}
+	
+	public void mInsert(){
+		SQLiteDatabase db = getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		cv.put("cusNo", "002");
+		cv.put("cusNa", "joy");
+		cv.put("cusPho", "22-333");
+		cv.put("cusAdd", "Japen");
+		
+		db.insert(Table_Name, null, cv);
+		db.close();
+	}
+	
+	public String FinRec(String CusNo){
+		// 1. 建立DB連線
+		SQLiteDatabase db = getReadableDatabase();
+		// 2. 建立SQL語句
+		//加入搜尋條件: " where CusNo like ?"
+		String sql = "select * from " + Table_Name + " where CusNo like ?";
+		//搜尋條件放入 array，可對應多個?參數
+		String[] args ={"%" + CusNo + "%"}; 
+		// 3. 執行 cursor. 作搜尋DB的動作.
+		//    cursor 內含有一個table, 根據cursor位置指向不同筆資料(row).
+		Cursor cursor = db.rawQuery(sql, args);
+		// [***] 讀取cursor指到 table的欄位數量 => 這裡數量是4, 因為有欄位CusNo/CusNa/CusPho/CusAddr.
+		int columnCount = cursor.getColumnCount();
+		
+		String fld = ""; 
+
+		if(cursor.getCount() != 0){
+			// 4. 指向某筆資料 
+			while(cursor.moveToNext()){
+									
+				//[***] 變數使用範圍.
+				//   根據宣告的位置, 決定可使用範圍.
+				//   {} 封閉範圍, 也就是說, {}外面的人看不到它的內部.
+//				String fld = ""; 
+						
+				// 5. 讀資料	
+				//   [***] cursor 讀一個String類型的資料, 根據輸入的欄位index(位置).
+				// 回傳的資料格式: "CusNo:CusNa"
+				fld += cursor.getString(0) + ":" + cursor.getString(1) + "\n";
+				
+//				for(int i = 0; i < columnCount; i++){
+//					fld += cursor.getString(i)  + ":";
+//				}
+			}
+		}
+		
+		// 6. 關掉cursor.
+		cursor.close();
+		// 7. 關掉 DB連線
+		db.close();
+		
+		return fld;		
+	}
+}
---
+++ Note_ch12_6Exercise/src/com/example/test_ch12_6ex/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.test_ch12_6ex;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private DBHelper dbHelper;
+	private EditText etNo;
+	private Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+       buildViews();
+       dbHelper = new DBHelper(this);
+       dbHelper.mInsert();
+    }
+    
+    public void buildViews(){
+    	 etNo = (EditText)findViewById(R.id.etIdNo);
+         btnOk = (Button)findViewById(R.id.btnIdSure);
+         btnOk.setOnClickListener(btnOkListener);
+    }
+    
+    //活動恢復作用時，呼叫此方法。作用是；如果 DBHelper不存在，則重新創建一個。
+    @Override
+	protected void onResume() {
+		super.onResume();
+		if(dbHelper == null){
+			dbHelper = new DBHelper(this);
+		}
+	}
+
+    //活動暫停時，呼叫此方法。作用是:關閉 DBHelper，並把 dbHelper 設為 null，釋放所占用的資源。
+    @Override
+	protected void onPause() {
+		super.onPause();
+		if(dbHelper != null){
+			dbHelper.close();
+			dbHelper = null;
+		}
+		
+	}
+
+	private OnClickListener btnOkListener = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			String result = "";
+			String cusNo = etNo.getText().toString().trim();
+			String rec = dbHelper.FinRec(cusNo);
+			if(rec.length() != 0){
+				result = "客戶資料:" + rec;
+			}else{
+				result = "無此資料:" + cusNo;
+			}
+			
+			Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+		}
+    };
+}//end class MainActivity. 
