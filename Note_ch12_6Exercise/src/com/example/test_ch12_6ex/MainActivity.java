a76ad123674516329ddc9f6ed7f149ae67e23752
Devon1129 <koistu0501@gmail.com>
2015-06-03 00:10:57 +0800

    Note -String的判斷。

--- Note_ch12_6Exercise/src/com/example/test_ch12_6ex/MainActivity.java
+++ Note_ch12_6Exercise/src/com/example/test_ch12_6ex/MainActivity.java
@@ -53,20 +53,33 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onClick(View v) {
-			String result = "";
-			
 			//**注意抓取順序:先存取抓到的 editText(cusNo)，
-			//再將 editText放入 method(FinRec)，得到的值，去做判斷。
-			String cusNo = etNo.getText().toString().trim();
-			String rec = dbHelper.FinRec(cusNo);
+			//    再將 editText放入 method(FinRec)，得到的值，去做判斷。
 			
-			if(rec.length() != 0){
-				result = "客戶資料:" + rec;
-			}else{
-				result = "無此資料:" + cusNo;
+			String cusNo = etNo.getText().toString().trim();
+		/*
+			判斷 String的可能性有三種:null、""(空字串)、"..."(有值)。
+			     做判斷時將可能性各別帶入條件式，導出最後結果是否與所想的一樣。
+			   e.g. if(cusNo != null && cusNo.length() != 0 )
+			   1. "...":
+			      (true && true)-> true
+			   2. "":
+			      (true && false)-> false
+			   3. null:
+			      (false && Exception)-> false(因為第一個判斷為 false，就不再判斷了)。
+		 * 
+		 */
+			if(cusNo != null && cusNo.length() != 0 ){
+				String rec = dbHelper.FinRec(cusNo);
+				String result;
+				if(rec != null){
+					result = "客戶資料:" + rec;
+				}else{
+					result = "無此資料:" + cusNo;
+				}	
+				Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
 			}
 			
-			Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
 		}
     };
 }//end class MainActivity. 
