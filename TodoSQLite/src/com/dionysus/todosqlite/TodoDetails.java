2e226e0000692440eeba14d20f52c88fbc8128ae
Haruhi.itoo <haruhi.itoo@gmail.com>
2015-10-20 13:25:07 +0800

    將detail的DB操作搬到button的event內, 及移除一些activity events, 讓流程比較簡單. 加入一些註解加強說明.

--- TodoSQLite/src/com/dionysus/todosqlite/TodoDetails.java
+++ TodoSQLite/src/com/dionysus/todosqlite/TodoDetails.java
@@ -12,36 +12,42 @@ import android.widget.Spinner;
 public class TodoDetails extends Activity{
 	private EditText mTitleText, mBodyText;
 	private Long mRowId;
-	private TodoDbAdapter mDbHelper;
+	private TodoDbAdapter mDBAdapter;
 	private Spinner mCategory;
 
 	@Override
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.todo_edit);
-		
-		mDbHelper = new TodoDbAdapter(this);
-		mDbHelper.open();
-		
+				
 		// Get views
 		mCategory = (Spinner)findViewById(R.id.category);
 		mTitleText = (EditText)findViewById(R.id.todo_edid_summary);
-		mBodyText = (EditText)findViewById(R.id.todo_edit_description);
+		mBodyText = (EditText)findViewById(R.id.todo_edit_description);	
 		
+		//1. 產生 DB adapter. 用來讀取/寫入DB.
+		mDBAdapter = new TodoDbAdapter(this);
+		mDBAdapter.open();
 		
-//		mRowId = null;
+		//2. 讀取從上一面畫傳進來的資料包,
+		//  若有資料, 則更新row id, 即[編輯]模式.
+		//  否則為[新增]模式.
 		Bundle extras = getIntent().getExtras();
 		if(extras != null){
 			mRowId = extras.getLong(TodoDbAdapter.KEY_ROWID);
 		}
-		populateFields();
+		// 填寫UI欄位.
+		populateFields();		
 		
+		//3. 按下[確認]鍵後, 將UI欄位裡的資料寫入DB.
 		Button confirmButton = (Button)findViewById(R.id.todo_edit_button);
 		confirmButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				setResult(RESULT_OK);
+				// 將UI欄位裡的資料寫入DB.
+				saveState();
 				finish();
 				// [***] finish()後, 會call onSaveInstanceState(...).
 			}
@@ -51,7 +57,7 @@ public class TodoDetails extends Activity{
 	private void populateFields() {
 		// [***] mRowId!=null 表示是編輯模式.
 		if(mRowId != null){
-			Cursor todo = mDbHelper.fetchTodo(mRowId);
+			Cursor todo = mDBAdapter.fetchTodo(mRowId);
 			startManagingCursor(todo);
 			String category = todo.getString(todo.getColumnIndexOrThrow(TodoDbAdapter.KEY_CATEGORY));
 			
@@ -71,27 +77,6 @@ public class TodoDetails extends Activity{
 		}
 	}
 	
-	// 在Activity 被殺掉之前, 系統會call這個method.
-	@Override
-	protected void onSaveInstanceState(Bundle outState){
-		super.onSaveInstanceState(outState);
-		saveState();
-		outState.putSerializable(TodoDbAdapter.KEY_ROWID, mRowId);
-	}
-	
-	@Override
-	protected void onPause() {
-		super.onPause();
-		saveState();
-		
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		populateFields();
-	}
-
 	private void saveState() {
 		String category = (String)mCategory.getSelectedItem();
 		String summary = mTitleText.getText().toString();
@@ -101,14 +86,35 @@ public class TodoDetails extends Activity{
 		// 如果沒有mRowId, 表示是一筆新todo.
 		// 如果有mRowId, 表示是編輯todo.
 		if(mRowId == null){
-			long id = mDbHelper.createTodo(category, summary, description);
+			long id = mDBAdapter.createTodo(category, summary, description);
 			if(id > 0){
 				mRowId = id;
 			}
 		}else{
-			mDbHelper.updateTodo(mRowId, category, summary, description);
+			mDBAdapter.updateTodo(mRowId, category, summary, description);
 		}
 	}
+	
+//	// 在Activity 被殺掉之前, 系統會call這個method.
+//	@Override
+//	protected void onSaveInstanceState(Bundle outState){
+//		super.onSaveInstanceState(outState);
+//		saveState();
+//		outState.putSerializable(TodoDbAdapter.KEY_ROWID, mRowId);
+//	}
+	
+//	@Override
+//	protected void onPause() {
+//		super.onPause();
+//		saveState();
+//		
+//	}
+
+//	@Override
+//	protected void onResume() {
+//		super.onResume();
+//		populateFields();
+//	}
 	
 }//end class TodoDetails.
 
--- TodoSQLite/src/com/dionysus/todosqlite/TodoOverview.java
+++ TodoSQLite/src/com/dionysus/todosqlite/TodoOverview.java
@@ -16,15 +16,15 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
-//import android.support.v7.internal.widget.AdapterViewCompat.AdapterContextMenuInfo;
+
 
 public class TodoOverview extends ListActivity{
 	private static final int ACTIVITY_CREATE = 0;
 	private static final int ACTIVITY_EDIT = 1;
 	private static final int DELETE_ID = Menu.FIRST + 1;
-	private TodoDbAdapter dbHelper;
+	private TodoDbAdapter mDbAdapter;
 	private Cursor cursor;
-	private Button btnTitle;
+	private Button mBtnCreateToDo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +32,41 @@ public class TodoOverview extends ListActivity{
 		setContentView(R.layout.todo_list);
 		//setDividerHeight(20); 設定 分隔線高度
 		this.getListView().setDividerHeight(20);
-		dbHelper = new TodoDbAdapter(this);
-		dbHelper.open();
 		
-		//程式的最外層，判斷DB內是否有資料，沒有再加入自訂的資料。
-		// Better solution: 檢查preference 檔. 是否是第一次開啟本app.
-		// Check whether default record needed.
-		Cursor cur = dbHelper.fetchAllTodos();
+		//1. 產生 DB adapter. 用來讀取/寫入DB.
+		mDbAdapter = new TodoDbAdapter(this);
+		mDbAdapter.open();
+
+		//2. 讀取所有todo
+		//2.1 先在這裡讀一次全部todo, 以檢查是否DB無資料並加入一筆default todo.
+		Cursor cur = mDbAdapter.fetchAllTodos();
 		if(cur.getCount() == 0)
 		{
-			dbHelper.createTodo("Reminder", "todo...", "This is first todo.");			
+			mDbAdapter.createTodo("Reminder", "todo...", "This is first todo.");			
 		}
 		cur.close();
 		
+		//2.2  讀取所有todo, 並填入至ListView.
 		fillData();
+		
+		//3.當app整個結束時, 要記得關掉DB連線, 即onDestroy()內做的事.
+		
+		//
+		// 其它View的event操作.
+		//
+		
 		// [***]註冊 ContextMenu到 ListActivity裡.
 		//      讓ListActivity 的ListView與 MENU做連接，就會出現長按 Menu.
 		//      所按的MENU會挾帶被選中的item's ID與position.
 		registerForContextMenu(getListView());
 		
-		btnTitle = (Button)findViewById(R.id.btnTitle);
-		btnTitle.setOnClickListener(btnListener);
+		mBtnCreateToDo = (Button)findViewById(R.id.btnTitle);
+		mBtnCreateToDo.setOnClickListener(mBtnCreateTodoListener);
+		
+		Toast.makeText(this, "v2.1版", Toast.LENGTH_LONG).show();
 	}
 
-	private OnClickListener btnListener = new OnClickListener(){
+	private OnClickListener mBtnCreateTodoListener = new OnClickListener(){
 
 		@Override
 		public void onClick(View v) {
@@ -80,11 +91,16 @@ public class TodoOverview extends ListActivity{
 			createTodo();
 			return true;
 		case DELETE_ID:
-//	     	 所按的MENU會挾帶被選中的item's ID與position.
-			AdapterContextMenuInfo info = (AdapterContextMenuInfo)item.getMenuInfo();
+	     	// 長按todo item會出現menu items, 
+			//   這時選的MENU item會挾帶被選中的item's ID與position.
 			
-			dbHelper.deleteTodo(info.id);
+			//1. 讀取memu item的資訊(內含被選中的item's ID與position)
+			AdapterContextMenuInfo info = (AdapterContextMenuInfo)item.getMenuInfo();
+			//2. 使用menu item的ID做為todo ID, 並在DB內刪除此筆資料.
+			mDbAdapter.deleteTodo(info.id);
+			//3. 讀取所有todo, 並顯示至listview.
 			fillData();
+			
 			Toast.makeText(TodoOverview.this, "delete action: " + info.id,  
 					Toast.LENGTH_SHORT).show();
 			break;
@@ -94,12 +110,6 @@ public class TodoOverview extends ListActivity{
 		}
 		return super.onMenuItemSelected(featureId, item);
 	}
-
-	private void createTodo() {
-		Intent i = new Intent(this, TodoDetails.class);
-		startActivityForResult(i, ACTIVITY_CREATE);
-	}
-	
 	
 	//ListView and view (row) on which was clicked, position and. 
 	// [*****] 這個ListView是使用SimpleCursorAdapter, 我們假設這個adapter所回傳的id, 
@@ -108,11 +118,14 @@ public class TodoOverview extends ListActivity{
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 		
+		//1. NEW 一個intent
 		Intent i = new Intent(this, TodoDetails.class);
+		//2. 加入todo id至資料包
 		i.putExtra(TodoDbAdapter.KEY_ROWID, id);
-		//Activity returns an result if called with startActivityForResult.
 		
-		startActivityForResult(i, ACTIVITY_EDIT);
+		//3. 啟動另一個activity, 並等待回傳的結果
+		//     (當另一個activity結束, 會call onActivityResult(...), 這個method會顯示全部todo)				
+		startActivityForResult(i, ACTIVITY_EDIT); //Activity returns an result if called with startActivityForResult.
 	}
 	
 	//called with the result of the other activity
@@ -122,11 +135,18 @@ public class TodoOverview extends ListActivity{
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
+		
+		//讀取所有todo, 並顯示至ListView.
 		fillData();
 	}
 
+	private void createTodo() {
+		Intent i = new Intent(this, TodoDetails.class);
+		startActivityForResult(i, ACTIVITY_CREATE);
+	}
+	
 	private void fillData() {
-		cursor = dbHelper.fetchAllTodos();
+		cursor = mDbAdapter.fetchAllTodos();
 		startManagingCursor(cursor);
 		
 		String[] from = new String[]{TodoDbAdapter.KEY_SUMMARY, TodoDbAdapter.KEY_CATEGORY};
@@ -143,14 +163,18 @@ public class TodoOverview extends ListActivity{
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
+		
+		//產生menu view, 這裡只有一個item, [DELETE].
 		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		if(dbHelper != null){
-			dbHelper.close();
+		
+		//記得關掉DB連線(即close() method內做的事)
+		if(mDbAdapter != null){
+			mDbAdapter.close();
 		}
 	}
 
